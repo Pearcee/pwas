d1b34cc9795fc097dcfaaa706d2fc43169a65468
steve pearce <sjpearce@gmail.com>
2024-01-22 10:49:10 +0000

    1

---
+++ pwa-1-start/src/assets/js/scripts.js
@@ -0,0 +1,39 @@
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+
+// function openCity(evt, cityName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
+
+
+console.log("Its me")
+
+// Used to toggle the menu on small screens when clicking on the menu button
+// function menuFn() {
+//   var x = document.getElementById("navDemo");
+//   if (x.className.indexOf("w3-show") == -1) {
+//     x.className += " w3-show";
+//   } else {
+//     x.className = x.className.replace(" w3-show", "");
+//   }
+// }
+
+closeSidebar();
+function openSidebar() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeSidebar() {
+  document.getElementById("mySidebar").style.display = "none";
+}
